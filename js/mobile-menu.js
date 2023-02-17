43a96eeb6b55fd86b2713d8ea5af56a3ae6905db
Vadimde88 <vadimde88@gmail.com>
2023-02-17 10:45:22 -0500

    JS fixed

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,12 +1,20 @@
-
 (() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
 
-  menuBtnRef.addEventListener("click", () => {
-    const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-    menuBtnRef.classList.toggle("is-active");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
-    mobileMenuRef.classList.toggle("is-open");
+  menuBtnRef.addEventListener('click', () => {
+    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+    menuBtnRef.classList.toggle('is-active');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+    mobileMenuRef.classList.toggle('is-open');
+  });
+
+  const links = mobileMenuRef.querySelectorAll('a');
+  links.forEach(link => {
+    link.addEventListener('click', () => {
+      mobileMenuRef.classList.remove('is-open');
+      menuBtnRef.setAttribute('aria-expanded', false);
+      menuBtnRef.classList.remove('is-active');
+    });
   });
 })();
